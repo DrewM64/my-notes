0ea402cbe45fc2b72feedd71b28cfa2d75ee3cba
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2025-09-08 12:54:36 -0400

    Added initial Tailwind CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>My Notes</h1>
-      </header>
-      <body>
-        <form>
-          <input type="text" placeholder="Add title" />
-          <input type="text" placeholder="Notes" />
-          <button type="submit">Add Note</button>
-        </form>
-        <section id="notes">
-          <div className="item">
-            <p className="item-title">Title: Item 1</p>
-            <p className="item-note">Note: Do this thing</p>
-            <button className="remove-button">X</button>
-          </div>
-          <div className="item">
-            <p className="item-title">Title: Item 2</p>
-            <p className="item-note">Note: Do this thing</p>
-            <button className="remove-button">X</button>
-          </div>
-          <div className="item">
-            <p className="item-title">Title: Item 3</p>
-            <p className="item-note">Note: Do this thing</p>
-            <button className="remove-button">X</button>
-          </div>
-        </section>
-      </body>
+    <div className="flex justify-center">
+      <div className="bg-cyan-200 w-1/2 py-4 px-8">
+        <header>
+          <h1 className="text-xl text-center">My Notes</h1>
+        </header>
+        <div className="flex flex-col">
+          <form id="add-note" className="self-center">
+            <input type="text" placeholder="Add title" />
+            <input type="text" placeholder="Notes" />
+            <button type="submit">Add Note</button>
+          </form>
+          <section id="notes">
+            <div className="item">
+              <p className="item-title">Title: Item 1</p>
+              <p className="item-note">Note: Do this thing</p>
+              <button className="remove-button">X</button>
+            </div>
+            <div className="item">
+              <p className="item-title">Title: Item 2</p>
+              <p className="item-note">Note: Do this thing</p>
+              <button className="remove-button">X</button>
+            </div>
+            <div className="item">
+              <p className="item-title">Title: Item 3</p>
+              <p className="item-note">Note: Do this thing</p>
+              <button className="remove-button">X</button>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
